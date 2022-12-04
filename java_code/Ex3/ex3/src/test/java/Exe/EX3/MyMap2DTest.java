674394d4c3443035ae624ab004ef586ad25d3e73
Chaim Averbach <Chaymi99@gmail.com>
2022-12-04 08:37:56 +0200

    Created JUnit test class

---
+++ java_code/Ex3/ex3/src/test/java/Exe/EX3/MyMap2DTest.java
@@ -0,0 +1,34 @@
+package Exe.EX3;
+
+import org.junit.jupiter.api.Test;
+
+public class MyMap2DTest {
+    @Test
+    public void testDrawSegment() {
+        // TODO: implement test
+    }
+    @Test
+    public void testDrawRect() {
+        // TODO: implement test
+    }
+    @Test
+    public void testDrawCircle() {
+        // TODO: implement test
+    }
+    @Test
+    public void testFill() {
+        // TODO: implement test
+    }
+    @Test
+    public void testShortestPath() {
+        // TODO: implement test
+    }
+    @Test
+    public void testShortestPathDist() {
+        // TODO: implement test
+    }
+    @Test
+    public void testNextGenGol() {
+        // TODO: implement test
+    }
+}

0be6fd33b6493f74ecc9442f1502a63d6b9ef896
Chaim Averbach <Chaymi99@gmail.com>
2022-12-21 08:49:33 +0200

    Added experiments main file

---
+++ java_code/Experiments/experiments/src/main/java/intro2cs_exercises/Main.java
@@ -0,0 +1,37 @@
+package intro2cs_exercises;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        double d = 5.0;
+        int i = 3;
+        int j = 5;
+        String s = "Hello";
+        Object o = new Object();
+        ArrayList<Object> al = new ArrayList<Object>();
+        al.add(d);
+        System.out.println(d);
+        al.add(i);
+        System.out.println(i);
+        al.add(j);
+        System.out.println(j);
+        al.add(s);
+        System.out.println(s);
+        al.add(o);
+        System.out.println(o);
+        System.out.println(differentClasses(al));
+    }
+
+    public static int differentClasses(ArrayList<?> al) {
+        Stream<?> s = Stream.of(al).map((Object o) -> { return o.getClass(); });
+        Set<Class<?>> found = new HashSet<Class<?>>();
+        for (Object o : al) {
+            found.add(o.getClass());
+        }
+        return found.size();
+    }
+}

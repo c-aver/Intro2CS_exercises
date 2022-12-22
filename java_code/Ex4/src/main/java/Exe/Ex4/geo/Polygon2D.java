57d2deb1c340597e0c04f91dd07831d039670197
Chaim Averbach <Chaymi99@gmail.com>
2022-12-23 01:44:07 +0200

    Added initial implementation of Polygon2D

--- java_code/Ex4/src/main/java/Exe/Ex4/geo/Polygon2D.java
+++ java_code/Ex4/src/main/java/Exe/Ex4/geo/Polygon2D.java
@@ -1,5 +1,7 @@
 package Exe.Ex4.geo;
 
+import java.util.ArrayList;
+
 /**
  * This class represents a 2D polygon, as in https://en.wikipedia.org/wiki/Polygon
  * This polygon can be assumed to be simple in terms of area and contains.
@@ -9,7 +11,21 @@ package Exe.Ex4.geo;
  *
  */
 public class Polygon2D implements GeoShapeable{
+	private ArrayList<Point2D> _points;
 
+	public Polygon2D() { //TODO: do we want this?
+		_points = new ArrayList<Point2D>();
+	}
+	public Polygon2D(ArrayList<Point2D> points) {
+		_points = points;
+	}
+	public void addPoint(Point2D p) {
+		_points.add(p);
+	}
+	public void removePoint(Point2D p) {
+		_points.remove(p);
+	}
+	
 	@Override
 	public boolean contains(Point2D ot) {
 		// TODO Auto-generated method stub
@@ -40,9 +56,9 @@ public class Polygon2D implements GeoShapeable{
 
 	@Override
 	public GeoShapeable copy() {
-		// TODO Auto-generated method stub
-		assert false : "Polygon2D is not implemented";
-		return null;
+		ArrayList<Point2D> copy = new ArrayList<Point2D>();
+		copy.addAll(_points);
+		return new Polygon2D(copy);
 	}
 
 	@Override
@@ -61,9 +77,7 @@ public class Polygon2D implements GeoShapeable{
 
 	@Override
 	public Point2D[] getPoints() {
-		// TODO Auto-generated method stub
-		assert false : "Polygon2D is not implemented";
-		return null;
+		return _points.toArray(new Point2D[0]);  // return the points arraylist converted to an array, the paramter is provided to determine the type of the array
 	}
 	
 }

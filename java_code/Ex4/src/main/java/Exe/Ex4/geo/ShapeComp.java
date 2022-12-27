90b10810eccd8bdfdd91d37c343adec8ab8c9b03
Chaim Averbach <Chaymi99@gmail.com>
2022-12-27 14:56:22 +0200

    Inital impl of ShapeComp

--- java_code/Ex4/src/main/java/Exe/Ex4/geo/ShapeComp.java
+++ java_code/Ex4/src/main/java/Exe/Ex4/geo/ShapeComp.java
@@ -13,26 +13,49 @@ import Exe.Ex4.GUI_Shapeable;
  *
  */
 public class ShapeComp implements Comparator<GUI_Shapeable>{
-	//////////add your code below ///////////
 	
-	
+	public static final Comparator<GUI_Shapeable> CompByTag = new ShapeComp(Ex4_Const.Sort_By_Tag);
+	public static final Comparator<GUI_Shapeable> CompByAntiTag = new ShapeComp(Ex4_Const.Sort_By_Anti_Tag);
+	public static final Comparator<GUI_Shapeable> CompByArea = new ShapeComp(Ex4_Const.Sort_By_Area);
+	public static final Comparator<GUI_Shapeable> CompByAntiArea = new ShapeComp(Ex4_Const.Sort_By_Anti_Area);
+	public static final Comparator<GUI_Shapeable> CompByPerimter = new ShapeComp(Ex4_Const.Sort_By_Perimeter);
+	public static final Comparator<GUI_Shapeable> CompByAntiPerimter = new ShapeComp(Ex4_Const.Sort_By_Anti_Perimeter);
 	public static final Comparator<GUI_Shapeable> CompByToString = new ShapeComp(Ex4_Const.Sort_By_toString);
+	public static final Comparator<GUI_Shapeable> CompByAntiToString = new ShapeComp(Ex4_Const.Sort_By_Anti_toString);
 	
 	private int _flag;
 	public ShapeComp(int flag) {
+		if (flag < 0 || flag > Ex4_Const.sortTypeCount)
+			throw new IllegalArgumentException("Illegal Shape Comprator flag");
 		_flag = flag;
-		
 	}
 	@Override
 	public int compare(GUI_Shapeable o1, GUI_Shapeable o2) {
-		int ans=0;
-		if(_flag == Ex4_Const.Sort_By_toString) {
-			ans = o1.toString().compareTo(o2.toString());
+		assert Ex4_Const.sortTypeCount == 8 : "Sort type check is not exhaustive";
+		if (_flag == Ex4_Const.Sort_By_Tag) {
+			return Integer.compare(o1.getTag(), o2.getTag());
 		}
-		//////////add your code below ///////////
-		
-		//////////////////////////////////////////
-		return ans;
+		if (_flag == Ex4_Const.Sort_By_Anti_Tag) {
+			return -Integer.compare(o1.getTag(), o2.getTag());
+		}
+		if (_flag == Ex4_Const.Sort_By_Area) {
+			return Double.compare(o1.getShape().area(), o2.getShape().area());
+		}
+		if (_flag == Ex4_Const.Sort_By_Anti_Area) {
+			return -Double.compare(o2.getShape().area(), o1.getShape().area());
+		}
+		if (_flag == Ex4_Const.Sort_By_Perimeter) {
+			return Double.compare(o1.getShape().perimeter(), o2.getShape().perimeter());
+		}
+		if (_flag == Ex4_Const.Sort_By_Anti_Perimeter) {
+			return -Double.compare(o2.getShape().perimeter(), o1.getShape().perimeter());
+		}
+		if (_flag == Ex4_Const.Sort_By_toString) {
+			return o1.toString().compareTo(o2.toString());
+		}
+		if (_flag == Ex4_Const.Sort_By_Anti_toString) {
+			return o2.toString().compareTo(o1.toString());
+		}
+		throw new IllegalArgumentException("Illegal Shape Comparator flag");
 	}
-
 }

4f4d14180f3ebfed92aeb9a536a0a6488e6568f4
Chaim Averbach <Chaymi99@gmail.com>
2023-01-01 09:55:23 +0200

    Added general test classes

---
+++ java_code/Ex4/src/test/java/Exe/Ex4/geo/GeoShapeableTest.java
@@ -0,0 +1,67 @@
+/*
+ * This class contains tests that are common among all GeoShapeables.
+ * TODO: does this need to be an actual JUnit test class?
+ */
+
+package Exe.Ex4.geo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class GeoShapeableTest {
+    // this is just in case I need it, should not use
+    void runAll(GeoShapeable geo) {
+        testArea(geo);
+        testContains(geo);
+        testCopy(geo);
+        testGetPoints(geo);
+        testMove(geo);
+        testPerimeter(geo);
+        testRotate(geo);
+        testScale(geo);
+        testToString(geo);
+    }
+
+    void testArea(GeoShapeable geo) {
+
+    }
+
+    void testContains(GeoShapeable geo) {
+
+    }
+
+    void testCopy(GeoShapeable geo) {
+
+    }
+
+    void testGetPoints(GeoShapeable geo) {
+
+    }
+
+    void testMove(GeoShapeable geo) {
+
+    }
+
+    void testPerimeter(GeoShapeable geo) {
+    }
+
+    void testRotate(GeoShapeable geo) {
+
+    }
+
+    void testScale(GeoShapeable geo) {
+        double ratio = Math.random();
+        double oldPerimeter = geo.perimeter(), expectedNewPerimeter = oldPerimeter * ratio;
+        double oldArea = geo.area(), expectedNewArea = oldArea * ratio * ratio;
+        geo.scale(Point2D.ORIGIN, ratio);
+        assertEquals(expectedNewArea, geo.area(), GeoTestConsts.EPS);
+        assertEquals(expectedNewPerimeter, geo.perimeter(), GeoTestConsts.EPS);
+
+    }
+
+    void testToString(GeoShapeable geo) {
+        String str = geo.toString();
+        String expectedName = geo.getClass().getSimpleName();
+        assertEquals(expectedName, str.split(",")[0]);
+    }
+}
---
+++ java_code/Ex4/src/test/java/Exe/Ex4/geo/GeoTestConsts.java
@@ -0,0 +1,6 @@
+package Exe.Ex4.geo;
+
+public class GeoTestConsts {
+    public static final double EPS = 0.001;
+    public static final double MAX_X = 10, MAX_Y = 10;
+}

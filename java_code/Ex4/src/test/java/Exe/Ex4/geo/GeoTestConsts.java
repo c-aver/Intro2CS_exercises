535c684aa38422054c72b046346ca54d91231610
Chaim Averbach <Chaymi99@gmail.com>
2023-01-07 23:57:08 +0200

    Changed constants

--- java_code/Ex4/src/test/java/Exe/Ex4/geo/GeoTestConsts.java
+++ java_code/Ex4/src/test/java/Exe/Ex4/geo/GeoTestConsts.java
@@ -3,6 +3,6 @@ package Exe.Ex4.geo;
 public class GeoTestConsts {
     public static final double EPS = 0.001;
     public static final double MAX_X = 10, MAX_Y = 10;
-    public static final int DARTS = 100000;
-    public static final int TESTS = 10;
+    public static final int DARTS = 10000;   // the number of "darts" to throw in the area test
+    public static final int TESTS = 1000;
 }

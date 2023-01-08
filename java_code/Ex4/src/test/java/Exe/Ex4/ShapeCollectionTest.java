55bfa38aafa46c3f882763f89ba24f5cb962629b
Chaim Averbach <Chaymi99@gmail.com>
2023-01-08 12:33:44 +0200

    Changed to RepeatedTest

--- java_code/Ex4/src/test/java/Exe/Ex4/ShapeCollectionTest.java
+++ java_code/Ex4/src/test/java/Exe/Ex4/ShapeCollectionTest.java
@@ -5,82 +5,82 @@
 
  package Exe.Ex4;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 
 public class ShapeCollectionTest {
-    @Test
+    @RepeatedTest(TestConsts.TESTS)
     void testAdd() {
         // TODO: implement
         assert false : "Not implemented";
     }
 
-    @Test
+    @RepeatedTest(TestConsts.TESTS)
     void testAddAll() {
         // TODO: implement
         assert false : "Not implemented";
     }
 
-    @Test
+    @RepeatedTest(TestConsts.TESTS)
     void testAddAt() {
         // TODO: implement
         assert false : "Not implemented";
     }
 
-    @Test
+    @RepeatedTest(TestConsts.TESTS)
     void testCopy() {
         // TODO: implement
         assert false : "Not implemented";
     }
 
-    @Test
+    @RepeatedTest(TestConsts.TESTS)
     void testGet() {
         // TODO: implement
         assert false : "Not implemented";
     }
 
-    @Test
+    @RepeatedTest(TestConsts.TESTS)
     void testGetBoundingBox() {
         // TODO: implement
         assert false : "Not implemented";
     }
 
-    @Test
+    @RepeatedTest(TestConsts.TESTS)
     void testLoad() {
         // TODO: implement
         assert false : "Not implemented";
     }
 
-    @Test
+    @RepeatedTest(TestConsts.TESTS)
     void testRemoveAll() {
         // TODO: implement
         assert false : "Not implemented";
     }
 
-    @Test
+    @RepeatedTest(TestConsts.TESTS)
     void testRemoveElementAt() {
         // TODO: implement
         assert false : "Not implemented";
     }
 
-    @Test
+    @RepeatedTest(TestConsts.TESTS)
     void testSave() {
         // TODO: implement
         assert false : "Not implemented";
     }
 
-    @Test
+    @RepeatedTest(TestConsts.TESTS)
     void testSize() {
         // TODO: implement
         assert false : "Not implemented";
     }
 
-    @Test
+    @RepeatedTest(TestConsts.TESTS)
     void testSort() {
         // TODO: implement
         assert false : "Not implemented";
     }
 
-    @Test
+    @RepeatedTest(TestConsts.TESTS)
     void testToString() {
         // TODO: implement
         assert false : "Not implemented";
--- java_code/Ex4/src/test/java/Exe/Ex4/gui/Ex4Test.java
+++ java_code/Ex4/src/test/java/Exe/Ex4/gui/Ex4Test.java
@@ -3,36 +3,38 @@
  * ID: 207486473
  */
 
- package Exe.Ex4.gui;
+package Exe.Ex4.gui;
 
-import org.junit.jupiter.api.Test;
+import Exe.Ex4.TestConsts;
+
+import org.junit.jupiter.api.RepeatedTest;
 
 public class Ex4Test {
-    @Test
+    @RepeatedTest(TestConsts.TESTS)
     void testActionPerformed() {
         // TODO: implement
         assert false : "Not implemented";
     }
 
-    @Test
+    @RepeatedTest(TestConsts.TESTS)
     void testGetInfo() {
         // TODO: implement
         assert false : "Not implemented";
     }
 
-    @Test
+    @RepeatedTest(TestConsts.TESTS)
     void testMouseClicked() {
         // TODO: implement
         assert false : "Not implemented";
     }
 
-    @Test
+    @RepeatedTest(TestConsts.TESTS)
     void testMouseMoved() {
         // TODO: implement
         assert false : "Not implemented";
     }
 
-    @Test
+    @RepeatedTest(TestConsts.TESTS)
     void testMouseRightClicked() {
         // TODO: implement
         assert false : "Not implemented";
